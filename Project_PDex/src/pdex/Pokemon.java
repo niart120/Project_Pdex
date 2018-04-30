96f131ad57c19b4cf9b49b1bd0da908813962b15
Tomoharu <Tomoharu@192.168.2.114>
2018-04-30 21:34:21 +0900

    added pokemon info database

--- Project_PDex/src/pdex/Pokemon.java
+++ Project_PDex/src/pdex/Pokemon.java
@@ -1,5 +1,97 @@
 package pdex;
 
 public class Pokemon {
+	//名前
+	private final String name;
+	//図鑑No.
+	private final int dexNo;
+	//id(通し番号)
+	private final int id;
+
+	//H種族値
+	private final int hBS;
+	//A種族値
+	private final int aBS;
+	//B種族値
+	private final int bBS;
+	//C種族値
+	private final int cBS;
+	//D種族値
+	private final int dBS;
+	//S種族値
+	private final int sBS;
+	//合計種族値
+	private final int sumBS;
+
+	//タイプ1
+	private final int type1;
+	//タイプ2
+	private final int type2;
+
+	public Pokemon(String n,int[] stat){
+		name = n;
+		dexNo = stat[0];
+		id = stat[1];
+
+		hBS = stat[2];
+		aBS = stat[3];
+		bBS = stat[4];
+		cBS = stat[5];
+		dBS = stat[6];
+		sBS = stat[7];
+		sumBS = stat[8];
+
+		type1 = stat[9];
+		type2 = stat[10];
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getDexNo() {
+		return dexNo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int gethBS() {
+		return hBS;
+	}
+
+	public int getaBS() {
+		return aBS;
+	}
+
+	public int getbBS() {
+		return bBS;
+	}
+
+	public int getcBS() {
+		return cBS;
+	}
+
+	public int getdBS() {
+		return dBS;
+	}
+
+	public int getsBS() {
+		return sBS;
+	}
+
+	public int getSumBS() {
+		return sumBS;
+	}
+
+	public int getType1() {
+		return type1;
+	}
+
+	public int getType2() {
+		return type2;
+	}
 
 }
