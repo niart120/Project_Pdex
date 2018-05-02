c5509427b44d33c41a55d2c2f317308668708c1d
Tomoharu <Tomoharu@192.168.2.114>
2018-05-03 00:53:42 +0900

    implemented a title window(temp)

--- Project_PDex/src/pdex/PDex.java
+++ Project_PDex/src/pdex/PDex.java
@@ -1,10 +1,34 @@
 package pdex;
 
-public class PDex {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class PDex extends Application{
 
 	public static void main(String[] args) {
-
+		Data test = new Data();
+		launch(args);
 
 	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		Parent root;
+		try{
+			root = FXMLLoader.load(getClass().getResource("Title.fxml"));
+			Scene scene = new Scene(root,400,600);
+			stage.setScene(scene);
+			stage.show();
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
 
 }
