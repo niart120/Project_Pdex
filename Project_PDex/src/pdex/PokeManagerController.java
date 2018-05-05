a7b4eb6a3261b568885a8653090bb9c280907b78
Tomoharu <Tomoharu@192.168.2.114>
2018-05-05 22:08:53 +0900

    added various controller

---
+++ Project_PDex/src/pdex/PokeManagerController.java
@@ -0,0 +1,36 @@
+package pdex;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+
+public class PokeManagerController extends Controller {
+
+	private static final Scene SCENE;
+	private static final TitleController instance;
+	static{
+		FXMLLoader fxmlLoader = new FXMLLoader(ClassLoader.getSystemResource("PokeManager.fxml"));
+		try{
+			fxmlLoader.load();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		Parent parent = fxmlLoader.getRoot();
+		Scene s = new Scene(parent);
+		s.setFill(Color.TRANSPARENT);
+		SCENE = s;
+		instance = fxmlLoader.getController();
+	}
+
+	public static Controller getInstance(){
+		return instance;
+	}
+
+	public void show(){
+		super.scene = SCENE;
+		notifyObservers();
+	}
+}
