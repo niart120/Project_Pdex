44c85275bceb3924f277e84f2ad403483f57843e
Tomoharu <Tomoharu@192.168.2.114>
2018-05-08 15:54:30 +0900

    added initial Data

--- Project_PDex/src/pdex/PokeDexController.java
+++ Project_PDex/src/pdex/PokeDexController.java
@@ -2,13 +2,26 @@ package pdex;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Color;
 
 public class PokeDexController{
+	@FXML private TableView<Pokemon> table;
+	@FXML private TableColumn nameCol;
+	@FXML private TableColumn type1Col;
+	@FXML private TableColumn type2Col;
+
+	private ObservableList<Pokemon> obsData;
+	private Data data = new Data();
 
 	private static final Scene SCENE;
 	private static final PokeDexController instance;
@@ -34,7 +47,22 @@ public class PokeDexController{
 		PDex.presentStage.setScene(SCENE);
 	}
 
-	public void onBackButtonClicked(ActionEvent event) {
+	public void initialize() {
+		obsData = FXCollections.observableArrayList();
+		table.setItems(obsData);
+
+		nameCol.setCellValueFactory(new PropertyValueFactory<Pokemon, String>("name"));
+		type1Col.setCellValueFactory(new PropertyValueFactory<Pokemon, String>("type1"));
+		type2Col.setCellValueFactory(new PropertyValueFactory<Pokemon, String>("type2"));
+	}
+
+	@FXML public void onBackButtonClicked(ActionEvent event) {
         TitleController.getInstance().show();
     }
+
+	@FXML public void onSearchButtonClicked(ActionEvent event) {
+		for(Pokemon poke: data.pokeData) {
+			obsData.addAll(poke);
+		}
+	}
 }
