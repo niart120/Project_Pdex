1c426dbfc7a3444f842d4111817104c7863e479e
Tomoharu <Tomoharu@192.168.2.114>
2018-05-12 21:26:34 +0900

    added some methods

--- Project_PDex/src/pdex/DamageCalculator.java
+++ Project_PDex/src/pdex/DamageCalculator.java
@@ -1,12 +1,46 @@
 package pdex;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DamageCalculator {
-	public int getDamage(int atkLV,int power,int atkStat,int defStat) {
+	private final int[][]  effectTable = new int[19][19];
+
+	public DamageCalculator() {
+		try {
+			File f = new File("./lib/effect.csv");
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String line;
+			int j=0;
+			while ((line = br.readLine()) != null) {
+				String[] tempData = line.split(",", 0);
+				for(int i=0;i<tempData.length;i++){
+					effectTable[i][j] = Integer.valueOf(tempData[i]);
+				}
+				j++;
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	private int getEffect(Move mv, DCPoke poke) {
+		return effectTable[mv.getType()][poke.getType1()]*effectTable[mv.getType()][poke.getType2()];
+	}
+
+	private int typeMatch(Move mv,DCPoke poke) {
+		return 2;
+	}
+
+	public int getDamage(int atkLV,int power,int atkStat,int defStat,Move mv,DCPoke atkPoke, DCPoke defPoke) {
 		/*ダメージ = 攻撃側のレベル × 2 ÷ 5 ＋ 2 → 切り捨て
 　		× 物理技(特殊技)の威力 × 攻撃側のこうげき(とくこう) ÷ 防御側のぼうぎょ(とくぼう) → 切り捨て
 　		÷ 50 ＋ 2 → 切り捨て
 　		× 乱数(0.85, 0.86, …… ,0.99, 1.00 の何れか) → 切り捨て
 		 */
-		return ((atkLV*2/5+2)*power*atkStat/defStat/50+2)*100/100;
+		return ((atkLV*2/5+2)*power*atkStat/defStat/50+2)*100/100*typeMatch(mv,atkPoke)/4*getEffect(mv,defPoke)/2;
 	}
 }
